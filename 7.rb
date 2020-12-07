fe519e022305426a2a9834cd25a4716f46d440dc
m7rk <>
2020-12-06 22:05:19 -0800

    7 pt 2

--- 7.rb
+++ 7.rb
@@ -1,11 +1,8 @@
 d = {}
-r = File.read("7.txt").split("\n").map{|r| d[r.split("contain")[0].gsub!(/s|\s+/, '')] = r.split("contain")[1].split(",").map{|m| m.gsub!(/[0-9]|s|\.|\s+/, '')}}
-
-k.keys.times do
-    d.each do |k,v| if (v.include?("hinygoldbag") || (v.any?{|f| valid.include?(f)}))
-            valid << k
-        end
-    end
+r = File.read("7.txt").split("\n").map{|r| d[r.split("contain")[0].gsub!(/s|\s+/, '')] = r.split("contain")[1].split(",").map{|m| m.gsub!(/s|\.|\s+/, '')}}
+valid = []
+def bcount(d,btype)
+    return 0 if btype == "nootherbag"
+    return 1 + d[btype].map{|m| m.to_i * bcount(d,m.gsub(/[0-9]+/,""))}.sum
 end
-
-puts valid.uniq.count
+puts bcount(d,"hinygoldbag") - 1
