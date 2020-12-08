67bea19f13105861bfa59313e96f1e963df5630a
m7rk <>
2020-12-08 02:10:48 -0800

    8 pt 2

--- 8.rb
+++ 8.rb
@@ -1,6 +1,26 @@
+def make_alts(rom)
+  out = []
+
+  rom.each_with_index do |d,i|
+    if(d[0] == "nop")
+      alt = Marshal.load(Marshal.dump(rom))
+      alt[i][0] = "jmp"
+      out << alt
+    end
+
+    if(d[0] == "jmp")
+      alt =  Marshal.load(Marshal.dump(rom))
+      alt[i][0] = "nop"
+      out << alt
+    end
+  end
+
+  return out
+end
+
 def exec(acc,ptr,hsh,rom)
-    puts hsh
-    return acc if hsh[ptr]
+    return false if hsh[ptr]
+    return acc if rom.count == ptr
     hsh[ptr] = true
     case rom[ptr][0]
       when "nop"
@@ -12,4 +32,7 @@ def exec(acc,ptr,hsh,rom)
     end
     return exec(acc, ptr,hsh,rom)
 end
-puts exec(0,0,{},File.readlines("8.txt").map{|m| [m.split(" ")[0],m.split(" ")[1].to_i]})
+
+alts = make_alts(File.readlines("8.txt").map{|m| [m.split(" ")[0],m.split(" ")[1].to_i]})
+
+puts alts.map{|x| exec(0,0,{},x)}.uniq[1]
