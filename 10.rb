b7a5d2e61f3a1c57e2e907b61cfe5e1f357249c6
m7rk <>
2020-12-10 07:36:59 -0800

    10 pt 2

--- 10.rb
+++ 10.rb
@@ -1,4 +1,34 @@
-d = (File.readlines("10.txt")).map(&:to_i).sort.each_cons(2).map {|b, a| b - a }
-puts (1+d.count(-3)) * (1+d.count(-1))
+d = ([0] + File.readlines("10.txt")).map(&:to_i).sort.reverse
+
+def to_directed_graph(d)
+  out = {}
+  d.each do |e|
+    out[e] = d.select{|v| v.between?(e+1, e+3)}
+  end
+  out
+end
+
+puts to_directed_graph(d)
 
 
+
+def paths_from_adapter(d, i, cache)
+  return cache[i] if cache[i]
+  return 1 if d[i].count == 0
+
+  sum = 0
+  count = d[i].each do |v|
+    val = 0
+    if (cache[v]) 
+      val = cache[v]
+    else
+      val = paths_from_adapter(d,v,cache)
+      cache[v] = val
+    end
+
+    sum += val
+  end
+  return sum
+end
+
+puts paths_from_adapter(to_directed_graph(d),0,{})
