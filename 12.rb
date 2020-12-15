7002d5c66ab60e0f6ac0e46405881785ac8d2906
m7rk <>
2020-12-14 19:23:54 -0800

    12 pt 2

--- 12.rb
+++ 12.rb
@@ -1,19 +1,34 @@
-dirs = ["N",[1,0],"W",[0,-1],"S",[-1,0],"E",[0,1]]
-facing = 6
+dirs = ["N",[-1,0],"W",[0,-1],"S",[1,0],"E",[0,1]]
 pos = [0,0]
+waypoint = [-1,10]
+
+def arysum(a,b)
+  return [a,b].transpose.map(&:sum) 
+end
+
+def aryrot(a,p)
+  return [a[1] - p[0], -a[0] - p[1]]
+end
+
+a = [24,-346]
+b = [21,244]
+
+
+
 File.readlines("12.txt").each do |l|
   d = dirs.find_index(l[0])
   if d
-    pos = [pos, dirs[d+1].collect {|n| n * l[1..-1].to_i }].transpose.map(&:sum) 
+    waypoint = arysum(waypoint, dirs[d+1].collect {|n| n * l[1..-1].to_i })
   elsif l[0] == "F"
-    pos = [pos, dirs[facing+1].collect {|n| n * l[1..-1].to_i }].transpose.map(&:sum) 
+    pos = arysum(pos, waypoint.collect {|n| n * l[1..-1].to_i })
   elsif l[0] == "R"
-    facing -= (2 * (l[1..-1].to_i/90))
+    (l[1..-1].to_i/90).times {waypoint = aryrot(waypoint,[0,0])}
   elsif l[0] == "L"
-    facing += (2 * (l[1..-1].to_i/90))
+    (3 * (l[1..-1].to_i/90)).times {waypoint =  aryrot(waypoint,[0,0])}
   end
-  facing = (facing + 8) % 8
   puts pos.inspect
+  puts waypoint.inspect
+  puts "--"
   
 end
 puts pos[0].abs + pos[1].abs
